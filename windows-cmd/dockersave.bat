c27edfd0ffda4a99b06d8a3746a335d21d2e3db9
ManojaD2004 <manojadkc2004@gmail.com>
2024-03-26 19:42:46 +0530

    Docker Stuff

--- windows-cmd/dockersave.bat
+++ windows-cmd/dockersave.bat
@@ -2,7 +2,7 @@
 
 if not exist "dockerstorage" (
     mkdir dockerstorage
-    docker save -o .\dockerstorage\macha-lang.tar $(docker image list --format "{{.Repository}}")
+    docker save -o .\dockerstorage\macha-lang.tar `docker image list --format "{{.Repository}}"`
 ) else (
-    docker save -o .\dockerstorage\macha-lang.tar $(docker image list --format "{{.Repository}}")
+    docker save -o .\dockerstorage\macha-lang.tar `docker image list --format "{{.Repository}}"`
 )
--- windows-cmd/dockerstart.dev.bat
+++ windows-cmd/dockerstart.dev.bat
@@ -2,6 +2,7 @@
 
 echo Starting Docker Compose in Development for Both Client and Server!!
 
+rem Redirecting "n" to the script
 docker-compose -f docker-compose.dev.yml up --build
 
 echo Stopping all Docker Development Containers
